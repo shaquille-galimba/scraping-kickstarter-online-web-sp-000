b14106ca607e9adcd15a7e5432e77b27dc62c967
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-02-29 10:17:40 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,7 +5,8 @@ require 'pry'
 def create_project_hash
   # write your code here
   html = File.read('fixtures/kickstarter.html')
-  # projects: kickstarter.css("li.project.grid_4").first
+  # projects: kickstarter.css("li.project.grid_4")
+  # title: project.css("h2.bbcard_name strong a").text
   kickstarter = Nokogiri::HTML(html)
   binding.pry
 end
